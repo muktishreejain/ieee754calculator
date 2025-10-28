5b35ab7293ead689e77054d7d13b4b4db7738c2c
Ayush Aryan <ayush.aryan014@gmail.com>
2025-10-29 00:04:40 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,10 +1,11 @@
 """
 Module: ops.py
-Performs IEEE-754 style addition and multiplication on 32-bit floats.
+
+Performs IEEE-754 style addition and multiplication on 32/64-bit floats.
 """
 
-from convert import float_to_ieee754, ieee754_to_float
-
+from convert import float_to_ieee754, ieee754_to_float, \
+                    float_to_ieee754_64, ieee754_to_float_64
 
 def add_floats(a: float, b: float) -> float:
     return a + b
@@ -21,3 +22,20 @@ def multiply_ieee754(bin_a: str, bin_b: str) -> str:
     a = ieee754_to_float(bin_a)
     b = ieee754_to_float(bin_b)
     return float_to_ieee754(a * b)
+
+# --- NEW 64-bit functions ---
+def add_floats_64(a: float, b: float) -> float:
+    return a + b
+
+def multiply_floats_64(a: float, b: float) -> float:
+    return a * b
+
+def add_ieee754_64(bin_a: str, bin_b: str) -> str:
+    a = ieee754_to_float_64(bin_a)
+    b = ieee754_to_float_64(bin_b)
+    return float_to_ieee754_64(a + b)
+
+def multiply_ieee754_64(bin_a: str, bin_b: str) -> str:
+    a = ieee754_to_float_64(bin_a)
+    b = ieee754_to_float_64(bin_b)
+    return float_to_ieee754_64(a * b)
